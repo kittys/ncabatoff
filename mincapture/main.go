3a741ae9dbde375522404a64921e8b07f582f643
Nick Cabatoff <Nick.Cabatoff@gmail.com>
2014-04-25 16:12:48 -0600

    add minimalist capture-to-screen program

---
+++ mincapture/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"code.google.com/p/ncabatoff/imgseq"
+	"code.google.com/p/ncabatoff/v4l"
+	"code.google.com/p/ncabatoff/vlib"
+)
+
+func main() {
+	cs := v4l.NewStream("/dev/video0", 0, "yuv", 640, 480, nil)
+	defer func() {
+		cs.Shutdown()
+	}()
+
+	imgdisp := make(chan []imgseq.Img, 1)
+	go vlib.StreamImages(imgdisp)
+
+	for simg := range cs.GetOutput() {
+		select {
+		case imgdisp <- []imgseq.Img{simg}:
+		default:
+		}
+	}
+}
